23198ac709d063819ccbfb8cd97376e5058195c4
pedro henrique <pedo.oliveira88@gmail.com>
2025-09-07 15:19:20 +0000

    teste

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-new Siema({ 
-    perPage: 2
-})
+const slider = new Siema()
+
+const buttonPrev = document.querySelector('.prev')
+const buttonNext = document.querySelector('.next')
+
+buttonPrev.addEventListener("click", () =>  slider.prev())
+
+buttonNext.addEventListener("click", () => slider.next())
