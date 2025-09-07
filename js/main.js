baf65cdab9d5cc3e0d4a9dc895087b96ee533edf
pedro henrique <pedo.oliveira88@gmail.com>
2025-09-07 16:22:29 +0000

    fim slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const slider = new Siema({
 perPage: 2,
 loop: true,
-duration: 200,
+duration: 500,
 }) 
 
 const buttonPrev = document.querySelector('.prev')
 const buttonNext = document.querySelector('.next')
 
-buttonPrev.addEventListener("click", () =>  slider.prev())
-buttonNext.addEventListener("click", () => slider.next())
+buttonPrev.addEventListener('click', () => {
+    // Se estiver no começo slide, vai pro final
+    if (slider.currentSlide === 0) {
+      slider.goTo(3);
+    } else {
+      slider.prev();
+    }
+  });
+  buttonNext.addEventListener('click', () => {
+    // Se estiver no final, vai pro começo
+    if (slider.currentSlide >= 3) {
+      slider.goTo(0);
+    } else {
+      slider.next();
+    }
+  });
